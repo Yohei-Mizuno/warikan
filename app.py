e5c84976ff255780db5ae56b509464fc5186f246
Yohei-Mizuno <mizyotama2525@gmail.com>
2020-08-22 20:41:30 +0900

    リファクタリング：日本語での説明を追加,ソースコードを整理.
    感想：
    ・ソースコードが見やすくなった
    ・日本語での説明により表示された結果がわかりやすくなった

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-total_amount = int(input())
+total_amount = int(input('合計金額を入力⇨'))
 
-total_number_of_people = int(input())
+total_number_of_people = int(input('合計人数を入力⇨'))
 
-print(total_amount)
 
-print(total_number_of_people)
-
-amount_per_person = total_amount / total_number_of_people
-
-print(amount_per_person)
+amount_per_person = total_amount // total_number_of_people
 
 fraction = total_amount % total_number_of_people
 
-print(fraction)
+print(f'1人当たりの金額⇨{amount_per_person}円\n端数⇨{fraction}円')
